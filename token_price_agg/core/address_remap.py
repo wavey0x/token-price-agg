a2bbf1c0a8a7d8df9067ccbcbf70ea4e12423b19
wavey0x <wavey0@proton.me>
2026-03-10 18:09:49 -0400

    add another remapping

--- token_price_agg/core/address_remap.py
+++ token_price_agg/core/address_remap.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 _REMAP_TABLE: dict[tuple[int, str], str] = {
     (1, "0xa3cc91589feedbbee0cfdc7404041e19cb00f110"): "0x4e3FBD56CD56c3e72c1403e103b45Db9da5B9D2B",
+    (1, "0x857ecd3faeac083303858167662c734f085c8c95"): "0x6B175474E89094C44Da98b954EedeAC495271d0F",
 }
 
 
